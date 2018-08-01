cbc205c02da97f0ffbd10cb8e19bdccb6cbaf763
Verisana <1902Grand@gmail.com>
2018-08-01 16:36:15 +0500

    Bugfix

--- btcbot/trader/seller_bot.py
+++ btcbot/trader/seller_bot.py
@@ -180,7 +180,7 @@ class LocalSellerBot():
                 return None
 
     def send_first_message(self, trade_obj):
-        qiwi = self.set_unused_qiwi()
+        qiwi = self.set_unused_qiwi(trade_obj)
         message = self.bot.greetings_text.format(qiwi.phone_number, trade_obj.reference_text)
         response = self.lbtc.post_message_to_contact(str(trade_obj.trade_id), message)
         if response.status_code == 200:
