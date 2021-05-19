5e40443bb648a1034255d491ca97ce7f07f5af0f
assanix <mail@0112.pro>
2021-05-19 17:57:47 +0500

    Last

--- autobot/CustomModelAdminMixin.py
+++ autobot/CustomModelAdminMixin.py
@@ -1,5 +1,4 @@
 class CustomModelAdminMixin(object):
-
     def __init__(self, model, admin_site):
         self.list_display = [field.name for field in model._meta.fields if field.name != "id"]
         self.fields = [field.name for field in model._meta.fields if field.name != "id"]
